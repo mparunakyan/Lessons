ebfa2000916fe993b59768ceb4d41e063271729e
mparunakyan <mparunakyan@gmail.com>
2021-01-16 00:25:52 +0300

    Исправлено и оптимизирована

--- Lesson1/Task4.py
+++ Lesson1/Task4.py
@@ -1,10 +1,7 @@
 mynum = int(input("Input number: "))
 sym = 0
 while len(str(mynum)) > 1:
+    mynum = mynum // 10
     if sym <= mynum % 10:
         sym = mynum % 10
-    mynum = mynum // 10
-    #print(mynum)
-    #print(sym)
-    #print(len(str(mynum)))
 print("Max number is: ", sym)
